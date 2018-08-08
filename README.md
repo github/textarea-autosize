93fedfbbf6b5ff81741dcef845edc2e83dd950d8
Joshua Peek <josh@joshpeek.com>
2018-08-08 15:02:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Textarea Autosize
+
+Autosizes textarea to size of it's contents.
+
+## Installation
+
+```
+$ npm install @github/textarea-autosize
+```
+
+## Usage
+
+```js
+import autosize from '@github/textarea-autosize'
+autosize(document.querySelector('textarea.foo'))
+```
+
+## Development
+
+```
+npm install
+npm test
+```
+
+## License
+
+Distributed under the MIT license. See LICENSE for details.
---
+++ textarea-autosize.js
@@ -0,0 +1,88 @@
+export default function autosize(textarea) {
+  let previousValue = null
+  let isUserResized = false
+
+  let x
+  let y
+  let height
+
+  function onUserResize(event) {
+    if (x !== event.clientX || y !== event.clientY) {
+      const newHeight = textarea.style.height
+      if (height && height !== newHeight) {
+        isUserResized = true
+        textarea.style.maxHeight = ''
+        textarea.removeEventListener('mousemove', onUserResize)
+      }
+      height = newHeight
+    }
+
+    x = event.clientX
+    y = event.clientY
+  }
+
+  const document = textarea.ownerDocument
+  const documentElement = document.documentElement
+
+  function overflowOffset() {
+    let offsetTop = 0
+    let el = textarea
+
+    while (el !== document.body && el !== null) {
+      offsetTop += el.offsetTop || 0
+      el = el.offsetParent
+    }
+
+    const top = offsetTop - document.defaultView.pageYOffset
+    const bottom = documentElement.clientHeight - (top + textarea.offsetHeight)
+    return {top, bottom}
+  }
+
+  function sizeToFit() {
+    if (isUserResized) return
+    if (textarea.value === previousValue) return
+    if (textarea.offsetWidth <= 0 && textarea.offsetHeight <= 0) return
+
+    const {top, bottom} = overflowOffset()
+    if (top < 0 || bottom < 0) {
+      return
+    }
+
+    const maxHeight = Number(getComputedStyle(textarea).height.replace(/px/, '')) + bottom
+    textarea.style.maxHeight = `${maxHeight - 100}px`
+
+    const container = textarea.parentElement
+    if (container instanceof HTMLElement) {
+      const containerHeight = container.style.height
+      container.style.height = getComputedStyle(container).height
+      textarea.style.height = 'auto'
+      textarea.style.height = `${textarea.scrollHeight}px`
+      container.style.height = containerHeight
+      height = textarea.style.height
+    }
+
+    previousValue = textarea.value
+  }
+
+  function onFormReset() {
+    isUserResized = false
+    textarea.style.height = ''
+    textarea.style.maxHeight = ''
+  }
+
+  textarea.addEventListener('mousemove', onUserResize)
+  textarea.addEventListener('input', sizeToFit)
+  textarea.addEventListener('change', sizeToFit)
+  const form = textarea.form
+  if (form) form.addEventListener('reset', onFormReset)
+  if (textarea.value) sizeToFit()
+
+  return {
+    unsubscribe() {
+      textarea.removeEventListener('mousemove', onUserResize)
+      textarea.removeEventListener('input', sizeToFit)
+      textarea.removeEventListener('change', sizeToFit)
+      if (form) form.removeEventListener('reset', onFormReset)
+    }
+  }
+}
